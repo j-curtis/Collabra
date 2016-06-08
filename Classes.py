047e87ec9bb222e65c86a9e5f92c9e3621a7670c
j-curtis <jcurtis2@u.rochester.edu>
2016-06-07 22:14:47 -0400

    Added search feature by keyword for database

--- Classes.py
+++ Classes.py
@@ -34,7 +34,7 @@ class Object(object):
 		#constructs an object given its JSON formatted string representation 
 		#uses jsonpickles decode method
 		return jsp.decode(JSONString)
-		
+
 #a derived class for Publications of various types 
 #we assume that all publications have an author, title, and year field
 class Publication(Object):
@@ -43,7 +43,8 @@ class Publication(Object):
 
 		self.objType = self.objType + ".pub"
 
-		self.keywords = []	#a list of words that can be used to identify the Publication 
+		self.keywords = []	#a list of words that can be used to identify the Publication
+							#all keywords are internalized as lowercase and are stored only once in the list  
 
 		#required fields for all publications
 		self.title = title
@@ -60,7 +61,15 @@ class Publication(Object):
 		self.authors.append(author)
 
 	def addKeyword(self,keyword):
-		self.keywords.append(keyword)
+		#adds a keyword but only if it isn't already in the keywords list
+		#makes lowercase before adding it so that all keywords are lowercase internally
+		if not keyword.lower() in self.keywords:
+			self.keywords.append(keyword.lower())
+
+	def addKeywords(self,list_of_keywords):
+		#adds each keyword in list_of_keywords
+		for keyword in list_of_keywords:
+			self.addKeyword(keyword)
 
 	def addNote(self,note):
 		self.notes.append(note)
@@ -137,13 +146,36 @@ class Database(Object):
 		#it is in the database, so we remove it 
 		del self.entries[objName]
 		
-	def getObject(self,objName):
-		#returns the object in the database with the given objName 
-		if objName in self.entries.keys():
-			return self.entries[objName]
+	def getObjects(self,list_of_objNames=[]):
+		#retuns a dictionary of objects in the database with an objName in the list 
+		return {k:v for k,v in self.entries.iteritems() if k in list_of_objNames}
 
-		#in this case, it is not in the database, so we return None 
-		return None 
+	def getPublications(self):
+		#this returns a dictionary of publications in the database 
+		#we do this by comparing the objType string to a set of Publication objTypes
+		#all publications classes have the first 8 characters of their objType as .obj.pub
+		return {k:v for k,v in self.entries.iteritems() if v.objType[:8] == ".obj.pub"} 
+
+	def searchPublicationsKeywords(self,list_of_keywords):
+		#returns a list of publication objNames (dictionary keys) in the database that have keywords in the given list of query words 
+		#list is sorted by number of matching keywords
+		#search is case insensitive
+
+		#we make the query set case insensitive by converting all the strings to lowercase
+		list_of_keywords_lower = map(lambda x: x.lower(), list_of_keywords)
+
+		#first we get a list of keys for all the publications
+		keys = self.getPublications().keys()
+
+		#we define a function that checks how many elements are in common between the query set and the keywords set 
+		def numInCommon(list1,list2):
+			return len(set(list1)&set(list2))
+		
+		#get keys whose value has some intersection with the query set 
+		keys = filter(lambda x: numInCommon(self.getObjects([x])[x].keywords,list_of_keywords) > 0, keys)
+		keys.sort(key=lambda x: numInCommon(self.getObjects([x])[x].keywords,list_of_keywords) ,reverse=True)
+
+		return keys
 
 """THESE CLASSES WILL BE USED TO ADD NEW FUNCTIONALITIES IN FURTHER BUILDS"""
 #a derived class for Author and Author-like objects
@@ -164,10 +196,16 @@ class Author(Object):
 
 
 #sample instances of objects useful for debugging
-_debug_obj = Object("debug_obj")
+_debug_obj = Object("_debug_obj")
+
 _debug_pub = Publication(objName="_debug_pub",title="A Publication",author="First Author",year=2016)
+_debug_pub.addKeywords(["test","debug"])
+
 _debug_article = Article(objName="_debug_article",title="An Article",author="First Author",year=2017,journal="A Journal",volume="300",number="40")
+_debug_article.addKeywords(["test","debug","article"])
+
 _debug_book = Book(objName="_debug_book",title="A Book",author="First Author",year=2018,publisher="A Publisher")
+_debug_book.addKeywords(["test","debug","book"])
 
 _debug_db = Database(objName="_debug_db")
 _debug_db.addObject(_debug_obj)
@@ -176,29 +214,9 @@ _debug_db.addObject(_debug_article)
 _debug_db.addObject(_debug_book)
 
 def main():
-	print _debug_obj
-	_debug_obj_JSON = _debug_obj.toJSONString()
-	_debug_obj_from_JSON = Object.fromJSONString(_debug_obj_JSON)
-	print _debug_obj.toJSONString()
-	print _debug_obj_from_JSON.toJSONString()
-	print
-	print _debug_pub
-	_debug_pub_JSON = _debug_pub.toJSONString()
-	_debug_pub_from_JSON = Publication.fromJSONString(_debug_pub_JSON)
-	print _debug_pub.toJSONString()
-	print _debug_pub_from_JSON.toJSONString()
-	print
-	print _debug_article
-	print _debug_article.toJSONString()
-	print
-	print _debug_book
-	print _debug_book.toJSONString()
-	print 
 	print _debug_db
-	_debug_db_JSON = _debug_db.toJSONString()
-	_debug_db_from_JSON = Database.fromJSONString(_debug_db_JSON)
-	print _debug_db.toJSONString()
-	print _debug_db_from_JSON.toJSONString()
+	print _debug_db.searchPublicationsKeywords(["fake"])
+
 
 
 if __name__ == "__main__":
